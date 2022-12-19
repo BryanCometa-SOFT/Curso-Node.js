7df45378ad70cb1c2306883817246a45f4347413
BryanCometa-SOFT <bscometa@misena.edu.co>
2022-12-19 18:00:29 -0500

    sección 1 y 2 completa

--- 01-HolaMundo/app.js
+++ 01-HolaMundo/app.js
@@ -1,9 +1,13 @@
 //Para ejecutar el js con node en el terminal sé usa "node nombreArchivo.js" 
 
+//Instalar  nodemon   npm install -g nodemon
+
+//ejecutar.. : nodemon archivo.js
+
 let name = "Bryan";
 console.log(name);
 
 name = "Stiven";
 console.log(name);
 
-console.log("Hello World "+name);
+console.log("Hola mundo "+name);
