fa0492af40c3d086de3337f463a407d4cbab79fe
BryanCometa-SOFT <bscometa@misena.edu.co>
2022-12-19 16:23:03 -0500

    Mi primer programa de node

---
+++ 01-HelloWorld/app.js
@@ -0,0 +1,9 @@
+//Para ejecutar el js con node en el terminal sé usa "node nombreArchivo.js" 
+
+let name = "Bryan";
+console.log(name);
+
+name = "Stiven";
+console.log(name);
+
+console.log("Hello World "+name);
